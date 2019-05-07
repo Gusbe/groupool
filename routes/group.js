9f8ba20b83051dad0634cf2ff9fb3d6d0dfbd9dd
gusbe <gusbertolo@gmail.com>
2019-05-07 15:03:59 +0200

    Recovery of parts of groups.js deleted

--- routes/group.js
+++ routes/group.js
@@ -9,12 +9,34 @@ router.get('/', (req, res, next) => {
 
 // GET '/group/create'
 router.get('/create', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('groups/group-create');
+  res.render('group/group-create');
 });
 
 // POST '/group/create'
-router.post('/create', (req, res, next) => {
-  // Validates that the field is correct. Renders the group information page.
+router.post('/create', ensureLogin.ensureLoggedIn(), (req, res) => {
+  const groupName = req.body.groupname;
+
+  if (groupName === '') {
+    res.render('group/group-create', { errorMessage: 'Write the group name' });
+  } else {
+    User.findOne({ username: req.user.username })
+      .then((user) => {
+        if (!user) {
+          res.render('group/group-create', { errorMessage: 'Problem with the user generating the group' });
+        } else {
+          const pin = Math.floor(Math.random() * 99999);
+          Group.create({ name: groupName, pin: pin, users: user })
+            .then(() => {
+              let data = {
+                groupName: groupName,
+                pin: pin
+              };
+              res.render('group/group-create', { data });
+            })
+            .catch((err) => console.log(err));
+        }
+      });
+  }
 });
 
 // GET '/group/join'
